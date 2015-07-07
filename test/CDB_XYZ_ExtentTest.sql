011a007f04866ba1e2a38c5c1bce797b5ef765f3
Paul Ramsey <pramsey@cleverelephant.ca>
2015-07-07 06:13:02 -0700

    Return test results in deterministic order (closes #96)

--- test/CDB_XYZ_ExtentTest.sql
+++ test/CDB_XYZ_ExtentTest.sql
@@ -5,5 +5,5 @@ inp AS ( select z0.z, r1.r as x, r2.r as y FROM zoom z0, range r1, range r2 WHER
 ext AS ( select x,y,z,CDB_XYZ_Extent(x,y,z) as g from inp )
 select X::text || ',' || Y::text || ',' || Z::text as xyz,
        st_xmin(g), st_xmax(g), st_ymin(g), st_ymax(g)
-       from ext;
+       from ext order by xyz;
 
