cfe7dbb6af58804b66641c5f1649fbd29c607d29
Andy Eschbacher <eschbacher@cartodb.com>
2015-09-24 15:53:34 -0400

    refactored

--- scripts-available/CDB_Albers.sql
+++ scripts-available/CDB_Albers.sql
@@ -9,30 +9,23 @@
 --  output: geometries of states in albers projections of the states
 -- 
 -- Projections:
--- 		- 
+--		- Lower 48 states: http://spatialreference.org/ref/sr-org/7965/
+-- 		- Alaska: http://spatialreference.org/ref/epsg/3338/
 --		- Puerto Rico: http://www.spatialreference.org/ref/epsg/nad83-puerto-rico-virgin-is/
 --		- Hawaii: http://epsg.io/102007
 
 
-CREATE OR REPLACE FUNCTION CDB_AlbersUSA (g geometry, state text, srid numeric DEFAULT 3857) RETURNS geometry as $$ 
+CREATE OR REPLACE FUNCTION CDB_Albers50 (g geometry, state text) RETURNS geometry as $$
 DECLARE
 	reply geometry;
-	new_srid INT;
-	alaska text[];
-	hawaii text[];
-	puertorico text[];
+	srid INT;
+	alaska text[] = '{"Alaska","AK","02"}'::text[];
+	hawaii text[] = '{"Hawaii","HI","15"}'::text[];
+	puertorico text[] = '{"Puerto Rico","PR","72"}'::text[];
 BEGIN
 	
-	alaska = '{"Alaska","AK","02"}'::text[];
-	hawaii = '{"Hawaii","HI","15"}'::text[];
-	puertorico = '{"Puerto Rico","PR","72"}'::text[];
-	
-	-- decide on SRID to use
-	IF srid = 3857 THEN 
-		new_srid = ST_SRID(g);
-	ELSE
-		new_srid = srid;
-	END IF;
+	-- convert to wgs84
+	IF ST_SRID(g) != 4326 THEN g = ST_Transform(g,4326); END IF;
 
 	EXECUTE 'SELECT
 	    ST_SetSRID(
@@ -82,10 +75,10 @@ BEGIN
 				ELSE
 					ST_Transform($1,42303)
   				END
-  				, $6
+  				, 3857
   			)'
 	INTO reply
-	USING g, state, alaska, hawaii, puertorico, new_srid;
+	USING g, state, alaska, hawaii, puertorico;
 
 	RETURN reply;
 
