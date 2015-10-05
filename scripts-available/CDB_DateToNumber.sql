186ed34ee54eb43a36d433078dc6ef8a768e9bb7
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-10-05 13:32:03 +0200

    Overloads CDB_DateToNumber() and adds test

--- scripts-available/CDB_DateToNumber.sql
+++ scripts-available/CDB_DateToNumber.sql
@@ -1,5 +1,21 @@
 -- Convert timestamp to double precision
 --
+CREATE OR REPLACE FUNCTION CDB_DateToNumber(input timestamp)
+RETURNS double precision AS $$
+DECLARE output double precision;
+BEGIN
+    BEGIN
+        SELECT extract (EPOCH FROM input) INTO output;
+    EXCEPTION WHEN OTHERS THEN
+        RETURN NULL;
+    END;
+RETURN output;
+END;
+$$
+LANGUAGE 'plpgsql' STABLE STRICT;
+
+-- Convert timestamp with time zone to double precision
+--
 CREATE OR REPLACE FUNCTION CDB_DateToNumber(input timestamp with time zone)
 RETURNS double precision AS $$
 DECLARE output double precision;
