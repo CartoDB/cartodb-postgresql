d352e1c46390cd105a25c5ebd53d9225f4f148b6
Javier Goizueta <jgoizueta@gmail.com>
2016-01-12 15:36:07 +0100

    Update Overviews tests

--- test/CDB_OverviewsTest.sql
+++ test/CDB_OverviewsTest.sql
@@ -8,12 +8,20 @@ SELECT _CDB_Aggregated_Attributes_Expression('base_bare_t'::regclass);
 SELECT _CDB_Aggregated_Attributes_Expression('base_bare_t'::regclass, 'tab');
 
 SELECT CDB_CreateOverviews('base_bare_t'::regclass);
+SELECT count(*) FROM base_bare_t_ov5;
+
 
 SELECT _CDB_Aggregable_Attributes_Expression('base_t'::regclass);
 SELECT _CDB_Aggregated_Attributes_Expression('base_t'::regclass);
 SELECT _CDB_Aggregated_Attributes_Expression('base_t'::regclass, 'tab');
 
 SELECT CDB_CreateOverviews('base_t'::regclass);
+SELECT count(*) FROM base_t_ov5;
+
+
+SELECT CDB_DropOverviews('base_bare_t'::regclass);
+SELECT CDB_DropOverviews('base_t'::regclass);
+SELECT count(*) FROM base_t_ov5;
 
 DROP TABLE base_bare_t;
 DROP TABLE base_t;
