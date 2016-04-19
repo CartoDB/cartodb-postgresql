331f7138c20fc00bafbd2aa053964fe4b689db46
abelvm <abel@cartodb.com>
2016-04-20 00:36:55 +0200

    stddev

---
+++ scripts-available/CDB_StdDevBins.sql
@@ -0,0 +1,19 @@
+--
+-- Calculate the DevStd bins for a given column
+--
+-- @param in_array A numeric array of numbers to determine the best
+--                   to determine the bin boundary
+--
+--
+-- Returns: upper edges of bins
+--
+--
+
+CREATE OR REPLACE FUNCTION CDB_EqualIntervalBins ( in_array NUMERIC[], breaks INT ) RETURNS NUMERIC[] as $$
+DECLARE
+    reply numeric[];
+BEGIN
+    WITH a as(SELECT unnest(in_array) e), b as( select a.e, TRUNC((AVG(a.e) - AVG(AVG(a.e)) OVER ()) / trunc((STDDEV(AVG(a.e)) OVER ())::numeric, 5) ) AS Bucket from a group by a.e ), c as( select max(a.e) as mx from b group by bucket order by bucket ) select array_agg(mx) into reply from c;
+    RETURN reply;
+END;
+$$ language plpgsql IMMUTABLE;
