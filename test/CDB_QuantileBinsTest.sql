0b5ca31931b44413a22f56376883b124a67bbf4f
Paul Norman <penorman@mac.com>
2016-04-27 12:27:51 -0700

    Add a QuantileBin test covering arrays with nulls

--- test/CDB_QuantileBinsTest.sql
+++ test/CDB_QuantileBinsTest.sql
@@ -1,5 +1,11 @@
 WITH data AS (
-    SELECT array_agg(x) x FROM generate_series(1,100) x 
+    SELECT array_agg(x::numeric) x FROM generate_series(1,100) x
         WHERE x % 5 != 0 AND x % 7 != 0
-    ) 
-SELECT unnest(CDB_QuantileBins(x, 7)) FROM data
+    )
+SELECT unnest(CDB_QuantileBins(x, 7)) FROM data;
+
+WITH data_nulls AS (
+    SELECT array_agg(CASE WHEN x % 4 != 0 THEN x ELSE NULL END) x FROM generate_series(1,100) x
+        WHERE x % 5 != 0 AND x % 7 != 0
+    )
+SELECT unnest(CDB_QuantileBins(x, 7)) FROM data_nulls;
