3122a0479d2bea14e12aa6ba6a5595168819af17
Paul Norman <penorman@mac.com>
2016-04-28 09:50:09 -0700

    Test behavior of binning fuctions with nulls
    All test results are based off of existing behavior, which doesn't
    always make sense (ref #247)

--- test/CDB_HeadsTailsBinsTest.sql
+++ test/CDB_HeadsTailsBinsTest.sql
@@ -1,5 +1,11 @@
 WITH data AS (
-    SELECT array_agg(x) x FROM generate_series(1,100) x 
+    SELECT array_agg(x::numeric) s FROM generate_series(1,100) x
         WHERE x % 5 != 0 AND x % 7 != 0
-    ) 
-SELECT round(unnest(CDB_HeadsTailsBins(x, 7)),2) FROM data
+    )
+SELECT round(unnest(CDB_HeadsTailsBins(s, 7)),2) FROM data;
+
+WITH data_nulls AS (
+    SELECT array_agg(CASE WHEN x % 2 != 0 THEN x ELSE NULL END::numeric) s FROM generate_series(1,100) x
+        WHERE x % 5 != 0 AND x % 7 != 0
+    )
+SELECT round(unnest(CDB_HeadsTailsBins(s, 7)),2) FROM data_nulls;
--- test/CDB_JenksBinsTest.sql
+++ test/CDB_JenksBinsTest.sql
@@ -1,5 +1,11 @@
 WITH data AS (
-    SELECT array_agg(x) x FROM generate_series(1,100) x 
+    SELECT array_agg(x::numeric) s FROM generate_series(1,300) x
         WHERE x % 5 != 0 AND x % 7 != 0
     ) 
-SELECT unnest(CDB_JenksBins(x, 7)) FROM data
+SELECT unnest(CDB_JenksBins(s, 7)) FROM data;
+
+WITH data_nulls AS (
+    SELECT array_agg(CASE WHEN x % 2 != 0 THEN x ELSE NULL END::numeric) s FROM generate_series(1,300) x
+        WHERE x % 5 != 0 AND x % 7 != 0
+    )
+SELECT unnest(CDB_JenksBins(s, 7)) FROM data_nulls;
