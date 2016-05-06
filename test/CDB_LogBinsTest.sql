7538c5cf3ba3023bf4011a849c4fbb1e70821a36
abelvm <abel@cartodb.com>
2016-05-06 12:01:08 +0200

    fixed texts

--- test/CDB_LogBinsTest.sql
+++ test/CDB_LogBinsTest.sql
@@ -2,4 +2,4 @@ WITH data AS (
     SELECT array_agg(x) x FROM generate_series(1,100) x
         WHERE x % 5 != 0 AND x % 7 != 0
     )
-SELECT unnest(CDB_LogBins(x, 7,'')) FROM data
+SELECT unnest(CDB_LogBins(x::numeric, 7,''::text)) FROM data
