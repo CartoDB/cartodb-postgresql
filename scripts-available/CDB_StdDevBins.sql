7209c370a1d02ea1ce134499a3c7d0494f703bc0
abelvm <abel@cartodb.com>
2016-05-06 11:46:15 +0200

    tests added, stddev minor fixes and formatting

--- scripts-available/CDB_StdDevBins.sql
+++ scripts-available/CDB_StdDevBins.sql
@@ -9,11 +9,27 @@
 --
 --
 
-CREATE OR REPLACE FUNCTION CDB_EqualIntervalBins ( in_array NUMERIC[]) RETURNS NUMERIC[] as $$
+CREATE OR REPLACE FUNCTION CDB_StdDevBins(in_array NUMERIC[]) RETURNS NUMERIC[] as $$
 DECLARE
     reply numeric[];
 BEGIN
-    WITH a as(SELECT unnest(in_array) e), b as( select a.e, TRUNC((AVG(a.e) - AVG(AVG(a.e)) OVER ()) / trunc((STDDEV(AVG(a.e)) OVER ())::numeric, 5) ) AS Bucket from a group by a.e ), c as( select max(a.e) as mx from b group by bucket order by bucket ) select array_agg(mx) into reply from c;
+    WITH a as(
+        SELECT unnest(in_array) e
+    ),
+    b as(
+        select a.e,
+        TRUNC((AVG(a.e) - AVG(AVG(a.e)) OVER ()) / trunc((STDDEV(AVG(a.e)) OVER ())::numeric, 5) ) AS Bucket
+        from a
+        group by a.e
+    ),
+    c as(
+        select
+        max(b.e) as mx
+        from b
+        group by bucket
+        order by bucket
+    )
+    select array_agg(mx) into reply from c;
     RETURN reply;
 END;
 $$ language plpgsql IMMUTABLE;
