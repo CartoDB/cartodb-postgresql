b462e969a13f2f120136beef9249afd4d90ae052
Antonio Zamorano <azamorano@cartodb.com>
2016-08-12 11:52:24 +0200

    Adding cache_tables to the create table too

--- scripts-available/CDB_AnalysisCatalog.sql
+++ scripts-available/CDB_AnalysisCatalog.sql
@@ -26,7 +26,9 @@ cartodb.cdb_analysis_catalog (
     -- last job modifying the node
     last_modified_by uuid,
     -- store error message for failures
-    last_error_message text
+    last_error_message text,
+    -- cached tables involved in the analysis
+    cache_tables regclass[] NOT NULL DEFAULT '{}'
 );
 
 -- This can only be called from an SQL script executed by CREATE EXTENSION
