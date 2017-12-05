2fe02d8154e7cb6ff61ac2ad41fef30c6d3b2dff
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-12-05 16:24:11 -0500

    fix alias of column

--- test/CDB_QuantileBinsTest.sql
+++ test/CDB_QuantileBinsTest.sql
@@ -6,11 +6,11 @@ SELECT unnest(CDB_QuantileBins(s, 10))
   FROM data;
 
 WITH data_nulls AS (
-    SELECT array_agg(x::numeric)
+    SELECT array_agg(x::numeric) AS s
       FROM (
         SELECT x FROM generate_series(0, 99) AS x
         UNION ALL
-        SELECT null AS s FROM generate_series(1, 10) AS x
+        SELECT null AS x FROM generate_series(1, 10) AS x
         ) _wrap
     )
 SELECT unnest(CDB_QuantileBins(s, 10))
