186ee37a570b2baaaafcb200ee4c6f3462f57f66
Raul Marin <git@rmr.ninja>
2019-10-03 16:54:55 +0200

    CDB_Username: Set secure search_path

--- scripts-available/CDB_Username.sql
+++ scripts-available/CDB_Username.sql
@@ -2,5 +2,9 @@
 CREATE OR REPLACE FUNCTION @extschema@.CDB_Username()
 RETURNS text
 AS $$
-  SELECT @extschema@.CDB_Conf_GetConf(CONCAT('api_keys_', session_user))->>'username';
-$$ LANGUAGE SQL STABLE PARALLEL SAFE SECURITY DEFINER;
+  SELECT @extschema@.CDB_Conf_GetConf(concat('api_keys_', session_user))->>'username';
+$$  LANGUAGE SQL
+    STABLE
+    PARALLEL SAFE
+    SECURITY DEFINER
+    SET search_path = @extschema@, pg_temp;
