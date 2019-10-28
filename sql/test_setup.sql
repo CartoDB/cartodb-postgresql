8ff0ad74af5b1ac65f7917dbadb2aa74c334efe6
Raul Marin <git@rmr.ninja>
2019-10-28 15:12:04 +0100

    Add regress target
    Runs the regress tests without installing the extension

--- sql/test_setup.sql
+++ sql/test_setup.sql
@@ -1,8 +1,14 @@
+\set ECHO none
+\set QUIET on
 SET client_min_messages TO error;
-CREATE EXTENSION cartodb CASCADE;
+CREATE EXTENSION postgis;
+CREATE EXTENSION plpythonu;
+CREATE SCHEMA cartodb;
+\i 'cartodb--unpackaged--@@VERSION@@.sql'
 CREATE FUNCTION public.cdb_invalidate_varnish(table_name text)
 RETURNS void AS $$
 BEGIN
   RAISE NOTICE 'cdb_invalidate_varnish(%) called', table_name;
 END;
 $$ LANGUAGE 'plpgsql';
+\set QUIET off
